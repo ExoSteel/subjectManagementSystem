16ceeffb11e35780c4a4d4afc9076cf773af8069
Silver <83893730+ExoSteel@users.noreply.github.com>
2024-05-25 12:02:56 +0000

    yep

--- main.py
+++ main.py
@@ -7,27 +7,27 @@ subjects = [] # 2D Array storing all currently loaded data
 while running:
     display.mainMenu()
     choice = input()
-    if not (typeCheck.check(choice, int) and rangeCheck.check(int(choice), 1, 7)):
+    if not validator.validate(choice, int, 1, 7):
         print("Wrong input, input must be an integer between 1 and 7!")
         continue
     if choice == "1":
         display.searchMenu()
         subjectCode = input()
-    if choice == "2":
+    elif choice == "2":
         display.addMenu()
         subjectCode = input()
-    if choice == "3":
+    elif choice == "3":
         display.editMenu()
         subjectCode = input()
-    if choice == "4":
+    elif choice == "4":
         display.deleteMenu()
         subjectCode = input()
-    if choice == "5":
+    elif choice == "5":
         display.loadMenu()
         subjectCode = input()
-    if choice == "6":
+    elif choice == "6":
         display.saveMenu()
-    if choice == "7":
+    else:
         print()
         print("Goodbye!")
         break
--- validator.py
+++ validator.py
@@ -3,44 +3,50 @@ import re
 
 def validate(data, type=None, lBound=None, uBound=None, length=0, format=None, existence=[]):
     flag = True
-    if data == None or data == "": # Presence Check
+    if data == None or data == "":                                      # Presence Check
         flag = False
     
     if type != None:
-        if type == int: # Type Check - Integer
-            flag = data.isdigit()
-        elif type == str: # Type Check - String
+        if type == int:                                                 # Type Check - Integer
+            if not data.isdigit():
+                return False
+        elif type == str:                                               # Type Check - String
             try:
-                flag = data.isalnum()
+                data.isalnum()
             except:
-                flag = False
+                return False
 
     if lBound != None and uBound != None:
-        flag = True if int(data) >= lBound and int(data) <= uBound else False # Range Check
+        if not (int(data) >= lBound and int(data) <= uBound):         # Range Check
+            return False   
     
     if length != 0:
-        flag = True if len(data) == length else False # Length Check
+        if len(data) != length:                                         # Length Check
+            return False                       
     
     if format != None:
-        flag = True if re.search(format, data) else False # Format Check (using RegEx)
+        if re.search(format, data) == None:                             # Format Check (using RegEx)
+            return False                   
 
-    if existence != []: # Existence Check
+    if existence != []:                                                 # Existence Check
         for exist in existence:
-            flag = True if data == exist else False
+            if data != exist:
+                return False
     
     return flag
 
-'''
+
 # Test
+print(validate("1")) # True
 print(validate(None)) # False
 print(validate("1", int)) # True
 print(validate("1", str)) # True
 print(validate("1", None, 0, 9)) # True
 print(validate("1", None, -5, 0)) # False
-print(validate("1", None, 0, 0, 1)) # True
-print(validate("1", None, 0, 0, 5)) # False
-print(validate("1", None, 0, 0, 0, "\d")) # True
-print(validate("1", None, 0, 0, 0, "\d\d")) # False
-print(validate("1", None, 0, 0, 0, None, ["1"])) # True
-print(validate("1", None, 0, 0, 0, None, ["1", "2"])) # False
-'''
+print(validate("1", None, 0, 9, 1)) # True
+print(validate("1", None, 0, 9, 5)) # False
+print(validate("1", None, 0, 9, 0, "\d")) # True
+print(validate("1", None, 0, 9, 0, "\d\d")) # False
+print(validate("1", None, 0, 9, 0, None, ["1"])) # True
+print(validate("1", None, 0, 9, 0, None, ["1", "2"])) # False
+
